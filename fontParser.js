c101d2364f6df3adf37a1a189141942838385c72
João Borges <sucrewar10@gmail.com>
2017-04-28 14:44:04 +0100

    Update fontParser.js
    added regex for replace hifen

--- fontParser.js
+++ fontParser.js
@@ -3,7 +3,7 @@ var formatString = ""
 for (var i=0;i<icons.length;i++){
     var name = (icons[i].querySelector(".mls").innerText.replace("icon-",""))
     var value = (icons[i].querySelector("input.unit").value)
-    formatString += 'static let '+name.replace('-', '_')+' = "\\u{'+value+'}"\n'
+    formatString += 'static let '+name.replace(/([-])/g, '_')+' = "\\u{'+value+'}"\n'
 }
 console.log(formatString)
 
@@ -15,6 +15,6 @@ var formatString = ""
 for (var i=0;i<icons.length;i++){
     var name = (icons[i].querySelector(".mls").innerText.replace("icon-",""))
     var value = (icons[i].querySelector("input.unit").value)
-    formatString += '<string name="'+name.replace('-', '_')+'">&#x'+value+'</string>\n'
+    formatString += '<string name="'+name.replace(/([-])/g, '_')+'">&#x'+value+'</string>\n'
 }
 console.log(formatString)
