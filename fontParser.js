cc052e019f7900e3ca5e2da12b2046a3c1a28e1e
João Borges <sucrewar10@gmail.com>
2017-04-28 14:36:15 +0100

    Update fontParser.js
    added font parser for android xml

--- fontParser.js
+++ fontParser.js
@@ -6,3 +6,15 @@ for (var i=0;i<icons.length;i++){
     formatString += 'static let '+name+' = "\\u{'+value+'}"\n'
 }
 console.log(formatString)
+
+
+//MARK parser ANDROID
+
+var icons = document.querySelectorAll(".glyph")
+var formatString = ""
+for (var i=0;i<icons.length;i++){
+    var name = (icons[i].querySelector(".mls").innerText.replace("icon-",""))
+    var value = (icons[i].querySelector("input.unit").value)
+    formatString += '<string name="'+name+'">&#x'+value+'</string>\n'
+}
+console.log(formatString)
